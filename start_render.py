36c67fee7bb2ed369d1d4db6fc1fd4528e7c9f36
Deploy <deploy@inflatablemodel.cn>
2026-06-08 18:35:01 +0800

    Remove config files, use API-only configuration

--- start_render.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-"""Render startup script"""
-import os
-from app import app
-
-if __name__ == "__main__":
-    port = int(os.getenv("PORT", 5002))
-    app.run(host="0.0.0.0", port=port)
